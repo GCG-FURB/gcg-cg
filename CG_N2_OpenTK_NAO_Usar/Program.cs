8b9e2bd269cda6a58fdcb271097a44e0bc15cc4f
Dalton Solano dos Reis <dalton.reis@gmail.com>
2020-09-24 18:13:34 -0300

    Usando C#ToPlantUML.

--- CG_N2_OpenTK_NAO_Usar/Program.cs
+++ CG_N2_OpenTK_NAO_Usar/Program.cs
@@ -1,21 +1,4 @@
-﻿/*
-@startuml
-  participant "OnLoad()" as A
-  participant "OnUpdateFrame()" as B
-  participant "OnRenderFrame()" as C
-activate A
-  A -> B:  __inicializa
-deactivate A
-activate B
-  B -> C: __desenhar
-  activate C
-    C --> B: __ajustes
-  deactivate B
-deactivate C
-@enduml
-*/
-
-// #define CG_Gizmo
+﻿// #define CG_Gizmo
 
 using System;
 using OpenTK;
