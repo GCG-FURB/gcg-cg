399157afa8338d82cdd64e9cd8a4460b6686fd8e
Dalton Solano dos Reis <dalton.reis@gmail.com>
2020-12-01 16:21:11 -0300

    Cor: valores.

--- CG_N4/Cubo.cs
+++ CG_N4/Cubo.cs
@@ -1,8 +1,3 @@
-
-/**
-  Autor: Dalton Solano dos Reis
-**/
-
 using OpenTK.Graphics.OpenGL;
 using CG_Biblioteca;
 
@@ -26,44 +21,43 @@ namespace gcgcg
     protected override void DesenharObjeto()
     {       // Sentido anti-horário
         GL.Begin(PrimitiveType.Quads);
-        // Face da frente
-        GL.Color3(System.Drawing.Color.Red);
+        // Face da frente (vermelho)
+        GL.Color3(1.0f,0.0f,0.0f);
         GL.Normal3(0, 0, 1);        
         GL.Vertex3(base.pontosLista[0].X, base.pontosLista[0].Y, base.pontosLista[0].Z);    // PtoA
         GL.Vertex3(base.pontosLista[1].X, base.pontosLista[1].Y, base.pontosLista[1].Z);    // PtoB
         GL.Vertex3(base.pontosLista[2].X, base.pontosLista[2].Y, base.pontosLista[2].Z);    // PtoC
         GL.Vertex3(base.pontosLista[3].X, base.pontosLista[3].Y, base.pontosLista[3].Z);    // PtoD
-        // Face do fundo
-        GL.Color3(System.Drawing.Color.Green);
+        // Face do fundo (verde)
+        GL.Color3(0.0f,1.0f,0.0f);
         GL.Normal3(0, 0, -1);
         GL.Vertex3(base.pontosLista[4].X, base.pontosLista[4].Y, base.pontosLista[4].Z);    // PtoE
         GL.Vertex3(base.pontosLista[7].X, base.pontosLista[7].Y, base.pontosLista[7].Z);    // PtoH
         GL.Vertex3(base.pontosLista[6].X, base.pontosLista[6].Y, base.pontosLista[6].Z);    // PtoG
         GL.Vertex3(base.pontosLista[5].X, base.pontosLista[5].Y, base.pontosLista[5].Z);    // PtoF
-        // Face de cima
-        GL.Color3(System.Drawing.Color.Blue);
+        // Face de cima (azul)
+        GL.Color3(0.0f,0.0f,1.0f);
         GL.Normal3(0, 1, 0);
         GL.Vertex3(base.pontosLista[3].X, base.pontosLista[3].Y, base.pontosLista[3].Z);    // PtoD
         GL.Vertex3(base.pontosLista[2].X, base.pontosLista[2].Y, base.pontosLista[2].Z);    // PtoC
         GL.Vertex3(base.pontosLista[6].X, base.pontosLista[6].Y, base.pontosLista[6].Z);    // PtoG
         GL.Vertex3(base.pontosLista[7].X, base.pontosLista[7].Y, base.pontosLista[7].Z);    // PtoH
-        // Face de baixo
-        GL.Color3(System.Drawing.Color.Yellow);
-        GL.Color4(0,0,255,2);
+        // Face de baixo (amarelo)
+        GL.Color3(1.0f,1.0f,0.0f);
         GL.Normal3(0, -1, 0);
         GL.Vertex3(base.pontosLista[0].X, base.pontosLista[0].Y, base.pontosLista[0].Z);    // PtoA
         GL.Vertex3(base.pontosLista[4].X, base.pontosLista[4].Y, base.pontosLista[4].Z);    // PtoE
         GL.Vertex3(base.pontosLista[5].X, base.pontosLista[5].Y, base.pontosLista[5].Z);    // PtoF
         GL.Vertex3(base.pontosLista[1].X, base.pontosLista[1].Y, base.pontosLista[1].Z);    // PtoB
-        // Face da direita
-        GL.Color3(System.Drawing.Color.Cyan);
+        // Face da direita (ciano)
+        GL.Color3(0.0f,1.0f,1.0f);
         GL.Normal3(1, 0, 0);
         GL.Vertex3(base.pontosLista[1].X, base.pontosLista[1].Y, base.pontosLista[1].Z);    // PtoB
         GL.Vertex3(base.pontosLista[5].X, base.pontosLista[5].Y, base.pontosLista[5].Z);    // PtoF
         GL.Vertex3(base.pontosLista[6].X, base.pontosLista[6].Y, base.pontosLista[6].Z);    // PtoG
         GL.Vertex3(base.pontosLista[2].X, base.pontosLista[2].Y, base.pontosLista[2].Z);    // PtoC
-        // Face da esquerda
-        GL.Color3(System.Drawing.Color.Magenta);
+        // Face da esquerda (magenta)
+        GL.Color3(1.0f,0.0f,1.0f);
         GL.Normal3(-1, 0, 0);
         GL.Vertex3(base.pontosLista[0].X, base.pontosLista[0].Y, base.pontosLista[0].Z);    // PtoA
         GL.Vertex3(base.pontosLista[3].X, base.pontosLista[3].Y, base.pontosLista[3].Z);    // PtoD
